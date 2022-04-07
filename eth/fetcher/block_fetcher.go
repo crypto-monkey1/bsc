754686cb71ff45c199790ec6ffa637864e7e2983
Ubuntu <ubuntu@ip-172-31-8-8.eu-west-1.compute.internal>
2022-04-07 07:05:58 +0000

    init

--- eth/fetcher/block_fetcher.go
+++ eth/fetcher/block_fetcher.go
@@ -809,7 +809,7 @@ func (f *BlockFetcher) notifyLastReceivedBlock(block *types.Block, url string) {
 		"gasUsed":             block.GasUsed(),
 		"receivedAtUnixMilli": block.ReceivedAt.UnixMilli(),
 		"receivedAtUTC":       block.ReceivedAt.UTC(),
-		"origin":              "ire2",
+		"origin":              "frank",
 	})
 	req, err := http.NewRequest("POST", url, bytes.NewReader(blockInJson))
 	if err != nil {
