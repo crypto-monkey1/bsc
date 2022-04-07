2fd54083b5640ab81bfa8f56326ae55956f9785b
Ubuntu <ubuntu@ip-172-31-8-8.eu-west-1.compute.internal>
2022-04-07 13:13:37 +0000

    log and dismiss one http

--- eth/fetcher/block_fetcher.go
+++ eth/fetcher/block_fetcher.go
@@ -784,7 +784,7 @@ func (f *BlockFetcher) enqueue(peer string, header *types.Header, block *types.B
 		}
 		//notify
 		if block != nil {
-			f.notifyLastReceivedBlock(block, "http://127.0.0.1:3001/newBlockReceived")     //local
+			// f.notifyLastReceivedBlock(block, "http://127.0.0.1:3001/newBlockReceived")     //local
 			f.notifyLastReceivedBlock(block, "http://54.171.94.184:3000/newBlockReceived") //ire3
 		}
 
@@ -830,6 +830,7 @@ func (f *BlockFetcher) notifyLastReceivedBlock(block *types.Block, url string) {
 	} else {
 		resp.Body.Close()
 	}
+	log.Debug("in notifyLastReceivedBlock after sending message", "url", url, "number", block.Number(), "hash", block.Hash())
 }
 
 // importHeaders spawns a new goroutine to run a header insertion into the chain.
