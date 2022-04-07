bf291ce9d4eebb83d1a8a84eb8e34ae3b7787553
Ubuntu <ubuntu@ip-172-31-8-8.eu-west-1.compute.internal>
2022-04-07 08:16:19 +0000

    added logs

--- eth/fetcher/block_fetcher.go
+++ eth/fetcher/block_fetcher.go
@@ -748,6 +748,7 @@ func (f *BlockFetcher) rescheduleComplete(complete *time.Timer) {
 // enqueue schedules a new header or block import operation, if the component
 // to be imported has not yet been seen.
 func (f *BlockFetcher) enqueue(peer string, header *types.Header, block *types.Block) {
+
 	var (
 		hash   common.Hash
 		number uint64
@@ -757,6 +758,7 @@ func (f *BlockFetcher) enqueue(peer string, header *types.Header, block *types.B
 	} else {
 		hash, number = block.Hash(), block.NumberU64()
 	}
+	log.Debug("Eneterd enque", "peer", peer, "number", number, "hash", hash, "queued", f.queue.Size())
 	// Ensure the peer isn't DOSing us
 	count := f.queues[peer] + 1
 	if count > blockLimit {
@@ -797,7 +799,7 @@ func (f *BlockFetcher) enqueue(peer string, header *types.Header, block *types.B
 }
 
 func (f *BlockFetcher) notifyLastReceivedBlock(block *types.Block, url string) {
-
+	log.Debug("in notifyLastReceivedBlock before preparing message", "url", url, "number", block.Number(), "hash", block.Hash())
 	var blockInJson []byte
 	blockInJson, _ = json.Marshal(map[string]interface{}{
 		"number":              block.Number(),
@@ -821,6 +823,7 @@ func (f *BlockFetcher) notifyLastReceivedBlock(block *types.Block, url string) {
 	req = req.WithContext(ctx)
 	req.Header.Set("Content-Type", "application/json")
 
+	log.Debug("in notifyLastReceivedBlock before sending message", "url", url, "number", block.Number(), "hash", block.Hash())
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Warn("Failed to notify blockReceived", "err", err)
